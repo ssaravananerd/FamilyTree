5c19d68d827f3ac4f6687a8d3941c5818289b02b
ssaravananerd <ssaravananerd@gmail.com>
2015-05-26 23:20:46 +0530

    First commit
    First commit

---
+++ FamilyTree/FamilyTree.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FamilyTree.Web.MvcApplication" Language="C#" %>
---
+++ FamilyTree/FamilyTree.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FamilyTree.Web.Startup))]
+namespace FamilyTree.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
